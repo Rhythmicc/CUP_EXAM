c77336d40d48048c7a547b271539ac6b6705e0e1
连浩丞 <RhythmLian@outlook.com>
2019-08-05 15:43:03 +0800

    repair install.sh

--- install(linux).sh
+++ install(linux).sh
@@ -2,20 +2,12 @@
 basedir=$(cd $(dirname "$0") || exit; pwd -P)
 chmod a+x "$basedir"/frontend.py
 chmod 777 "$basedir"/start.sh
-file1="$HOME/.bashrc"
+file="$HOME/.bashrc"
 # shellcheck disable=SC2088
-file2="$HOME/.profile"
-if [ ! -f "$file1" ]; then
-  touch "$HOME/.bashrc"
+if [ ! -f "$file" ]; then
+  file="$HOME/.profile"
 fi
 
-if [ ! -f "$file2" ]; then
-  touch "$HOME/.profile"
-fi
-
-sed '$a\alias exam="$basedir/start.sh"' ~/.bashrc
-sed '$a\alias exam="$basedir/start.sh"' ~/.profile
+sed '$a\alias exam="$basedir/start.sh"' "$file"
 # shellcheck disable=SC1090
-source ~/.bashrc
-# shellcheck disable=SC1090
-source ~/.profile
+source "$file"
